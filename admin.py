83a3a74a9995147e94c0d053ceb7333eb8780438
Fakea15 <75936459+Fakea15@users.noreply.github.com>
2021-08-18 20:22:22 +0530

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Post, Message, Profile
+
+# models as data center
+admin.site.register(Post)
+admin.site.register(Message)
+admin.site.register(Profile)
